f603d239f8b498f0cb78bc3324ec5b00c2f8bf64
gtaddei <gilles.taddei@ge.com>
2017-04-19 17:42:25 +0200

    made it simpler by removing the useless filenames in datanode and datasub

--- dataSubs.py
+++ dataSubs.py
@@ -1,10 +1,9 @@
 class dataSubs:
-    def __init__(self, nb, name, freq, filename):
+    def __init__(self, nb, name, freq):
         self.nb = nb
         self.name = name
         self.freq = freq
         self.i = 0
-        self.filename = filename
 
     def __iter__(self):
         return self
@@ -29,11 +28,3 @@ class dataSubs:
             str += sub
         str += self.get_ending()
         return str
-
-    def printer(self):
-        with open(self.filename, "w") as f:
-            f.write(self.get_beginning())
-            for sub in next(self):
-                f.write(sub)
-            f.write(self.get_ending())
-            #f.write(self.toString)
